503d149ddddab4ad46bc1cba6ddf84aaa2e7395e
Emmanuel Ariza <ejariza@gmail.com>
2017-08-16 17:22:14 -0500

    Now data is rendered

--- js/CsvTableCtrl.js
+++ js/CsvTableCtrl.js
@@ -2,13 +2,14 @@
 angular.module('snippet', [])
 .controller('CsvTableCtrl', ["$scope", "$http", function($scope, $http){
 	//$scope.test = ":v";
-	$http.get("../data/tabla1.csv").then(function(response) {
+	$http.get("https://raw.githubusercontent.com/cier-centro/edu-adultos/master/data/tabla1.csv").then(function(response) {
 		var rows = response.data.split("\n").map(function(rowString){
 			return rowString.split(",");
 		});
-		$scope.header = rows[0];
+		$scope.t1 = {};
+		$scope.t1.header = rows[0];
 		console.log(rows);
-		$scope.data = rows.splice(1);
+		$scope.t1.data = rows.splice(1);
 	}, function(response, error){
 		console.log(response);
 	})
