3b5db3ddaa4b834fe6c7c778801f8161ea7ea4a4
Kgtoledoc <kgtoledoc19@gmail.com>
2017-08-18 16:43:13 -0500

    Nuevos cambios en Controlador

--- js/CsvTableCtrl.js
+++ js/CsvTableCtrl.js
@@ -13,17 +13,19 @@ angular.module('snippet', [])
 	}, function(response, error){
 		console.log(response);
 	})
-}]).
+}])
 .controller('CsvTable4Ctrl', ["$scope", "$http", function($scope, $http){
 	//$scope.test = ":v";
 	$http.get("https://raw.githubusercontent.com/cier-centro/edu-adultos/master/data/tabla4.csv").then(function(response) {
 		var rows = response.data.split("\n").map(function(rowString){
-			return rowString.split(",");
+			return rowString.split(',');
 		});
 		$scope.t1 = {};
-		$scope.t1.header = rows[0];
+
 		console.log(rows);
-		$scope.t1.data = rows.splice(1);
+		$scope.t1.data = rows.map(function(row){
+			return [row[0], row[1], (row[2] + row[3]).replace(/"/g, ''), row[4]]
+		})
 	}, function(response, error){
 		console.log(response);
 	})
