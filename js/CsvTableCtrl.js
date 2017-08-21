36f006df3d51eb3cfc502c9239072540983a7f37
kgtoledoc <kgtoledoc19@gmail.com>
2017-08-21 18:54:50 -0500

    Modificación modelos flexibles

--- js/CsvTableCtrl.js
+++ js/CsvTableCtrl.js
@@ -30,3 +30,17 @@ angular.module('snippet', [])
 		console.log(response);
 	})
 }])
+.controller('CsvTable5Ctrl', ["$scope", "$http", function($scope, $http){
+	//$scope.test = ":v";
+	$http.get("https://raw.githubusercontent.com/cier-centro/edu-adultos/master/data/tabla5.csv").then(function(response) {
+		var rows = response.data.split("\n").map(function(rowString){
+			return rowString.split(",");
+		});
+		$scope.t1 = {};
+		$scope.t1.header = rows[0];
+		console.log(rows);
+		$scope.t1.data = rows.splice(1);
+	}, function(response, error){
+		console.log(response);
+	})
+}])
